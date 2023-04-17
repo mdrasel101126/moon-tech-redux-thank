7ecaf6558fc3373b45499f86b1ec5804370b9937
mdrasel101126 <mdrasel101126@gmail.com>
2023-04-17 19:24:03 +0600

    second commit

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { BiListPlus } from "react-icons/bi";
 import { useDispatch } from "react-redux";
-import { ADD_TO_CART } from "../redux/actionTypes/actionTypes";
 import { addToCart } from "../redux/actionCreators/productActions";
+import { RiDeleteBin5Fill } from "react-icons/ri";
+import { useLocation } from "react-router-dom";
 
 const ProductCard = ({ product }) => {
   //console.log(product);
+  const location = useLocation();
+  //console.log("Location: ", location);
   const dispatch = useDispatch();
   return (
     <div
@@ -29,18 +32,30 @@ const ProductCard = ({ product }) => {
         </ul>
       </div>
       <div className="flex gap-2 mt-5">
-        <button
-          onClick={() => dispatch(addToCart(product))}
-          className="bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold"
-        >
-          Add to cart
-        </button>
-        <button
-          title="Add to wishlist"
-          className="bg-indigo-500  py-1 px-2 rounded-full"
-        >
-          <BiListPlus className="text-white" />
-        </button>
+        {location.pathname === "/cart" && (
+          <button
+            onClick={() => dispatch(addToCart(product))}
+            className="bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold"
+          >
+            <RiDeleteBin5Fill />
+          </button>
+        )}
+        {location.pathname === "/" && (
+          <button
+            onClick={() => dispatch(addToCart(product))}
+            className="bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold"
+          >
+            Add to cart
+          </button>
+        )}
+        {location.pathname === "/" && (
+          <button
+            title="Add to wishlist"
+            className="bg-indigo-500  py-1 px-2 rounded-full"
+          >
+            <BiListPlus className="text-white" />
+          </button>
+        )}
       </div>
     </div>
   );
