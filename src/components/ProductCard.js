ee21a844f4a79432f332656c010ef41264a9a435
mdrasel101126 <mdrasel101126@gmail.com>
2023-04-18 23:45:21 +0600

    third commit

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { BiListPlus } from "react-icons/bi";
 import { useDispatch } from "react-redux";
-import { addToCart } from "../redux/actionCreators/productActions";
+import {
+  addToCart,
+  removeToCart,
+} from "../redux/actionCreators/productActions";
 import { RiDeleteBin5Fill } from "react-icons/ri";
 import { useLocation } from "react-router-dom";
 
@@ -12,10 +15,15 @@ const ProductCard = ({ product }) => {
   const dispatch = useDispatch();
   return (
     <div
-      className="shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900"
+      className="shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900 relative"
       key={product._id}
     >
       <div className="h-52 w-52 mx-auto">
+        {location.pathname === "/cart" && (
+          <div className="w-6 h-6 rounded-full bg-blue-500 text-white text-center absolute -right-3 -top-3">
+            <p>{product.quantity}</p>
+          </div>
+        )}
         <img src={product.image} alt={product.model} />
       </div>
       <h1 className="font-bold text-center">{product.model}</h1>
@@ -34,7 +42,7 @@ const ProductCard = ({ product }) => {
       <div className="flex gap-2 mt-5">
         {location.pathname === "/cart" && (
           <button
-            onClick={() => dispatch(addToCart(product))}
+            onClick={() => dispatch(removeToCart(product))}
             className="bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold"
           >
             <RiDeleteBin5Fill />
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -5,18 +5,41 @@ const initialState = {
 };
 
 const productReducer = (state = initialState, action) => {
+  let selectedProduct = state.cart.find(
+    (product) => product._id === action.payload._id
+  );
   switch (action.type) {
     case ADD_TO_CART:
+      if (selectedProduct) {
+        selectedProduct.quantity = selectedProduct.quantity + 1;
+        let newCart = state.cart.filter(
+          (product) => product._id !== selectedProduct._id
+        );
+        return {
+          ...state,
+          cart: [...newCart, selectedProduct],
+        };
+      }
       return {
         ...state,
-        cart: [...state.cart, action.payload],
+        cart: [...state.cart, { ...action.payload, quantity: 1 }],
       };
     case REMOVE_FROM_CART:
+      let newCart = state.cart.filter(
+        (product) => product._id !== selectedProduct._id
+      );
+      if (selectedProduct.quantity > 1) {
+        selectedProduct.quantity = selectedProduct.quantity - 1;
+        let index = state.cart.indexOf(selectedProduct);
+        newCart.splice(index, 0, selectedProduct);
+        return {
+          ...state,
+          cart: newCart,
+        };
+      }
       return {
         ...state,
-        cart: state.cart.filter(
-          (product) => product._id !== action.payload._id
-        ),
+        cart: [...newCart],
       };
     default:
       return state;
